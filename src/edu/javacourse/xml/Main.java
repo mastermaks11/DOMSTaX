233f3a136353c0cf9f9944db77a90ba82f0d9486
Denis Bobko <webmasterstar@mail.ru>
2018-06-23 18:45:24 +0400

    XML

---
+++ src/edu/javacourse/xml/Main.java
@@ -0,0 +1,33 @@
+package edu.javacourse.xml;
+
+import java.util.logging.LogManager;
+
+import edu.javacourse.xml.Hospital;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Created by user on 19.06.2018.
+ */
+public class Main {
+    public static void main(String[] args) throws ParseException, IOException, SAXException, ParserConfigurationException {
+        RegistrePatient registrePatient = new RegistrePatient();
+        try {
+            System.setProperty("java.until.logging.config.file", "logger.properties");
+            LogManager.getLogManager().readConfiguration();
+
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        }
+
+        registrePatient.registre();
+
+    }
+}
